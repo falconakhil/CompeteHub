2989655e29990286b4f8cc2023936076dcef7777
akhil <akhil.s2004@protonmail.com>
2025-03-10 16:31:41 +0530

    Added delete and profile

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path,include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import SignUp
+from .views import SignUp, UserProfile, DeleteUser
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('signup/',SignUp.as_view(),name='signup'),
+    path('profile/', UserProfile.as_view(), name='user-profile'),
+    path('delete/', DeleteUser.as_view(), name='delete'),
 ]
